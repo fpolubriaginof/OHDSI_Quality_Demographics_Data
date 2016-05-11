a20f2efc996d3aa9a5e149330c70a86f671f4d17
fpolubriaginof <fpolubriaginof@gmail.com>
2016-05-11 16:27:38 -0400

    Add files via upload

---
+++ OHDSI_SQL_Server_race_ethnicity_data_collection.sql
@@ -0,0 +1,16 @@
+###SQL Server
+
+#Overall statistics
+
+SELECT race_concept_id, ethnicity_concept_id, CASE WHEN count(*) > 10 THEN count(*) ELSE 10 END as cnt from
+[database_name].[schema_name].person p
+group by  race_concept_id, ethnicity_concept_id
+
+#Race and ethnicity by year of first visit in visit_occurrence table
+
+SELECT race_concept_id, ethnicity_concept_id, CASE WHEN count(*) > 10 THEN count(*) ELSE 10 END as cnt,year(first_visits.visit_start_date) from
+(SELECT person_id, min(visit_start_date) as visit_start_date
+  FROM [database_name].[schema_name].[visit_occurrence] vo
+  group by vo.person_id) first_visits
+left join [database_name].[schema_name].person p on p.person_id = first_visits.person_id
+group by  race_concept_id, ethnicity_concept_id, year(first_visits.visit_start_date)
